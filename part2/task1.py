4cd9d35a36c9c8e3551228aa3e28ef1f3a1bfb3c
Nikudanngo <satoru_rutosa@icloud.com>
2022-11-02 02:44:01 +0900

    task1考察途中

--- part2/task1.py
+++ part2/task1.py
@@ -2,19 +2,32 @@ import cv2
 
 # cv2.blurする
 img_benitengu = cv2.imread("img/input.jpg")
-img_benitengu_blur = cv2.blur(img_benitengu, (5, 5))
+img_benitengu_blur = cv2.blur(img_benitengu, (3, 3))
 
 cv2.imwrite("img/blur_benitengu.jpg", img_benitengu_blur)
 
 # # cv2.GaussianBlurする
 img_benitengu = cv2.imread("img/input.jpg")
-img_benitengu_GaussianBlur = cv2.GaussianBlur(img_benitengu, (5, 5), 0)
+img_benitengu_GaussianBlur = cv2.GaussianBlur(img_benitengu, (11, 11), 1)
 
 cv2.imwrite("img/GaussianBlur_benitengu.jpg", img_benitengu_GaussianBlur)
 
 
 # # cv2.bilateralFilterする
 img_benitengu = cv2.imread("img/input.jpg")
-img_benitengu_bilateralFilter = cv2.bilateralFilter(img_benitengu, 9, 75, 75)
+img_benitengu_bilateralFilter = cv2.bilateralFilter(img_benitengu,12, 12, 1)
 
 cv2.imwrite("img/bilateralFilter_benitengu.jpg", img_benitengu_bilateralFilter)
+
+# 上記では三つの平滑化処理を行ったが,それぞれの特徴を考察する。
+
+# cv2.blurは三つの平滑化の中で最も均等にぼかすことができるフィルターであると考えられる。
+
+# cv2.GaussianBlurは中心から外側に向かってぼかすことができるフィルターであると考えられる。
+
+
+# cv2.bilateralFilterは色の近い画素同士をぼかすことができるフィルターであると考えられる。
+# つまり、エッジをキープしたままぼかすことができる。
+# さらに、フィルターをかける前とかけた後で画像のサイズを比較したとこと、
+# かけた後の画像のサイズがかける前の画像のサイズよりも大きくなっていることがわかった。
+
