ae02fc81fc49daeac1127f9fabdff10bfe7ffec4
Nikudanngo <satoru_rutosa@icloud.com>
2022-11-02 20:04:31 +0900

    part2 finish

--- part2/task2.py
+++ part2/task2.py
@@ -6,7 +6,14 @@ import cv2
 
 img_benitengu = cv2.imread("img/input.jpg",0)
 img_benitengu_sobel = cv2.Sobel(img_benitengu, cv2.CV_32F, 1, 0, 3)
-img_benitengu_convertScaleAbs = cv2.convertScaleAbs(img_benitengu_sobel)
-
 cv2.imwrite("img/sobel_benitengu.jpg", img_benitengu_sobel)
+
+img_benitengu_convertScaleAbs = cv2.convertScaleAbs(img_benitengu_sobel, alpha=1, beta=1.0)
 cv2.imwrite("img/convertScaleAbs_benitengu.jpg", img_benitengu_convertScaleAbs)
+
+# SobelフィルタとconvertScaleAbsを使うことでどのよな結果が得られるか考察する
+# convertScaleAbsは画像のコントラストを調整する関数である。
+# 第二引数にalphaを設定することで、コントラスト変更のパラーメータとして調整することができる。
+# SobelフィルタとconvertScaleAbsを使うことで、エッジを検出し、コントラストを上げることで
+# エッジを強調することができる。
+
