6079b85d7a0042761b97defd170abad9972519c6
Nikudanngo <satoru_rutosa@icloud.com>
2022-11-02 21:01:56 +0900

    task3 finish

--- part2/task3.py
+++ part2/task3.py
@@ -1,29 +1,30 @@
 import cv2
 import numpy as np
-img1_src = cv2.imread('img/dog.jpg', 0)
+dogImage = cv2.imread('img/dog.jpg', 0)
 
 value = 150
 
-img_src = img1_src.copy()
+dogCopyImage = dogImage.copy()
 
-img_src[img1_src < value] = 0
-img_src[img1_src >= value] = 255
+dogCopyImage[dogImage < value] = 0
+dogCopyImage[dogImage >= value] = 255
 
-#cv2_imshow(img_src)
+toThreshold = cv2.threshold(dogImage, 200, 255, cv2.THRESH_BINARY)[1]
 
-img_tmp = cv2.Sobel(img_src, cv2.CV_32F, 1, 0, 3)
-img_dst = cv2.convertScaleAbs(img_tmp)
+element4 = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8)
+element8 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
 
-element4 = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], np.uint8) # 4 近傍
-element8 = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8) # 8 近傍
-n = 25
+toErode = cv2.erode(toThreshold, element4, iterations = 2)
+# toDilate = cv2.dilate(toThreshold, element8, iterations = 2)
 
-img_dst = cv2.erode(img_src, element4, iterations = 1)
-#img_dst = cv2.dilate(img_src, element8, iterations = 1)
 
-for i in range(n):
-  img_dst = cv2.dilate(img_dst, element4, iterations = 1)
-for i in range(n):
-  img_dst = cv2.erode(img_dst, element8, iterations = 1)
+toDilate = cv2.dilate(toErode, element4, iterations = 1)
+# toErode = cv2.erode(toDilate, element8, iterations = 1)
 
-cv2.imwrite('img/answer_3.jpg', img_dst)
+cv2.imwrite('img/answer_3.jpg', toDilate)
+
+# ここでは、Sobelフィルタを使ってエッジを検出し、convertScaleAbsでコントラストを上げることで
+# エッジを強調している。さらに、膨張処理または収縮処理を行うことで犬全体の輪郭を切り抜くような処理をおこなっている。
+# ここでは犬を綺麗に形どるために精密な切り取りを行うために第四近傍を用いる。
+# 今回では、n=2としている。iterationsで繰り返しおこなう回数を設定することができる。
+# また綺麗に形どるために、膨張処理を行った後に収縮処理を行うことで、犬の輪郭を切り抜いている。
